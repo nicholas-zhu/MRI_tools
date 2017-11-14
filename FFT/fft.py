289d70a82f27f85592fb3126a0e1ecf56692f859
xucheng.zhu@berkeley.edu <xucheng.zhu@berkeley.edu>
2017-11-14 11:38:33 -0800

    add FT folder

---
+++ FFT/fft.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pyfftw as pfft
+
+class fft:
+    def __init__(self, shape, axes = (0,1,2)):
+        self.shape = shape
+        self.axes = axes
+    
+    def FT(self,a):
+        b = pfft.interfaces.numpy_fft.fftn(a,axes=self.axes)
+        b = pfft.interfaces.numpy_fft.fftshift(b,axes=self.axes)
+        return b
+    
+    def IFT(self,a):
+        b = pfft.interfaces.numpy_fft.ifftshift(a,axes=self.axes)
+        b = pfft.interfaces.numpy_fft.ifftn(b, axes=self.axes)
+        return b
