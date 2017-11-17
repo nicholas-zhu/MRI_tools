75b1fd97a86365cbac9bfd2cab7ad8cc6e317c04
xucheng.zhu@berkeley.edu <xucheng.zhu@berkeley.edu>
2017-11-17 09:08:11 -0800

    add zpad2 func

--- util/process_3d.py
+++ util/process_3d.py
@@ -10,3 +10,13 @@ def zpad(img_arr, pad_size=[0,0,0], mode='constant'):
     img_arr_n = np.pad(img_arr, p_size, mode)
     return img_arr_n
 
+def zpad2(img_arr, r_size, mode='constant'):
+    img_size = np.array(img_arr.shape)
+    p_sizeL = np.round((np.array(r_size)-img_size)/2)
+    p_sizeR = np.float((np.array(r_size)-img_size)/2)
+    p_size  = np.stack([p_sizeL,p_sizeR],axis=1) 
+    p_size = tuple(map(tuple,psize))
+    
+    img_arr_n = np.pad(img_arr, p_size, mode)
+    return img_arr_n
+    
