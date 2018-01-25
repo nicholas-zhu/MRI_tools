2fbcd19627abac38ea7b6d41fc6680db5a5d6ad7
xucheng.zhu@berkeley.edu <xucheng.zhu@berkeley.edu>
2018-01-25 11:14:28 -0800

    add fftshift

--- FFT/fft.py
+++ FFT/fft.py
@@ -7,11 +7,13 @@ class fft:
         self.axes = axes
     
     def FT(self,a):
-        b = pfft.interfaces.numpy_fft.fftn(a,axes=self.axes)
+        b = pfft.interfaces.numpy_fft.ifftshift(a,axes=self.axes)
+        b = pfft.interfaces.numpy_fft.fftn(b,axes=self.axes)
         b = pfft.interfaces.numpy_fft.fftshift(b,axes=self.axes)
         return b
     
     def IFT(self,a):
         b = pfft.interfaces.numpy_fft.ifftshift(a,axes=self.axes)
         b = pfft.interfaces.numpy_fft.ifftn(b, axes=self.axes)
+        b = pfft.interfaces.numpy_fft.fftshift(b, axes=self.axes)
         return b
