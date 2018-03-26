377c453d6b4f537e04687d1c0b36568f59662fa8
xucheng.zhu@berkeley.edu <xucheng.zhu@berkeley.edu>
2018-03-26 08:14:14 -0700

    Update QSM functions

--- QSM/qsm.py
+++ QSM/qsm.py
@@ -3,29 +3,50 @@ from FFT import fft
 from util import *
 from scipy import optimize
 
+A = fft.fft(shape=1,axes=(0,1,2))
+
 def Ball_filter(r, voxelsize):
     yy,xx,zz = np.meshgrid(np.arange(-r,r+1),np.arange(-r,r+1),np.arange(-r,r+1))
     xx = xx*voxelsize[0]
     yy = yy*voxelsize[1]
     zz = zz*voxelsize[2]
     v2 = xx**2 + yy**2 + zz**2 + np.spacing(1)
-    B_kernal = v2<=(r**2+r/3)&v2>.1
+    # B_kernal = v2<=(r**2+r/3)&v2>.1
+    B_kernal = v2<=(r**2+r/3)
     B_kernal = B_kernal/np.sum(B_kernal)
     return B_kernal
 
-def SMVFiltering(Phase,radius, voxelsize = [1,1,1], pad_size = [6,6,6]):
+def SMVFiltering(Phase,radius,Mask, voxelsize = [1,1,1], pad_size = [6,6,6]):
     phase_pad = process_3d.zpad(Phase,pad_size)
+    Mask_pad = process_3d.zpad(Mask,pad_size)
     kernal = Ball_filter(radius,voxelsize)
-    k_pad = process_3d.zpad2(kernal,phase_pad.shape[0:2])
-    A = fft.fft(shape=1,axes=(0,1,2))
+    k_pad = process_3d.zpad2(kernal,phase_pad.shape[0:3])
+    
     if phase_pad.ndim>3 :
         kernal = np.expand_dims(kernal, axis=3)
-    phase_L = A.IFT(A.FT(k_pad)*A.FT(phase_pad))
-    phase_H = phase_pad - phase_L
-    phase_L = process_3d.crop(phase_L,pad_size)
+        Mask_pad = np.expand_dims(Mask_pad, axis=3)
+    
+    Mask_padf = np.abs(A.IFT(A.FT(k_pad)*A.FT(Mask_pad)))>.99
+    phase_L = A.IFT(A.FT(k_pad)*A.FT(phase_pad))*Mask_padf
+    phase_H = (phase_pad - phase_L)*Mask_padf
+    Mask_f = process_3d.crop(Mask_padf,pad_size)
+    phase_L = process_3d.crop(phase_L,pad_size)+(np.expand_dims(Mask, axis=3)-Mask_f)*Phase
     phase_H = process_3d.crop(phase_H,pad_size)
-    return phase_L, phase_H
+    return phase_L, Mask_f, phase_H
 
+def V_Sharp(Phase, ROI_Mask, resolution, svm_radius = None):
+    
+    if svm_radius is None:
+        svm_radius = range(10)
+        
+    Phase_f = Phase
+    for radius in svm_radius:
+        Phase_f, Mask_f, Phase_h = SMVFiltering(Phase_f, radius)
+        if radius == svm_radius[0]:
+            Filtered_Mask = Mask_f 
+    
+    return Phase_f, Filtered_Mask
+    
 def HARPERELLA(Phase,Mask,pad_size,voxel_size):
     
     yy, xx, zz = np.meshgrid(np.arange(0, raw_phase.shape[1]),
@@ -39,7 +60,7 @@ def HARPERELLA(Phase,Mask,pad_size,voxel_size):
     ik2 = 1/k2
     
     LP = A.IFT(k2*A.FT(Phase))
-    Mask1 = SVMFiltering(Mask,3)
+    # Mask1 = SVMFiltering(Mask,3)
     MaskRim = 2*Mask - (Mask1>.99)
     b = A.IFT(ik2*A.FT(Mask*LP))*MaskRim
     
