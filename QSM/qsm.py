a0600b335af46978a9328eb3c58796f18b79a487
xucheng.zhu@berkeley.edu <xucheng.zhu@berkeley.edu>
2018-03-26 09:35:33 -0700

    Update: SVM function

--- QSM/qsm.py
+++ QSM/qsm.py
@@ -17,20 +17,22 @@ def Ball_filter(r, voxelsize):
     return B_kernal
 
 def SMVFiltering(Phase,radius,Mask, voxelsize = [1,1,1], pad_size = [6,6,6]):
+    Phase = np.expand_dims(Phase, axis=3)
     phase_pad = process_3d.zpad(Phase,pad_size)
     Mask_pad = process_3d.zpad(Mask,pad_size)
     kernal = Ball_filter(radius,voxelsize)
     k_pad = process_3d.zpad2(kernal,phase_pad.shape[0:3])
     
     if phase_pad.ndim>3 :
-        kernal = np.expand_dims(kernal, axis=3)
+        k_pad = np.expand_dims(k_pad, axis=3)
         Mask_pad = np.expand_dims(Mask_pad, axis=3)
+        
     
-    Mask_padf = np.abs(A.IFT(A.FT(k_pad)*A.FT(Mask_pad)))>.99
-    phase_L = A.IFT(A.FT(k_pad)*A.FT(phase_pad))*Mask_padf
+    Mask_padf = np.real(A.IFT(A.FT(k_pad)*A.FT(Mask_pad)))>.99
+    phase_L = np.real(A.IFT(A.FT(k_pad)*A.FT(phase_pad))*Mask_padf)
     phase_H = (phase_pad - phase_L)*Mask_padf
     Mask_f = process_3d.crop(Mask_padf,pad_size)
-    phase_L = process_3d.crop(phase_L,pad_size)+(np.expand_dims(Mask, axis=3)-Mask_f)*Phase
+    phase_L = process_3d.crop(phase_L,pad_size)+(np.expand_dims(Mask, axis=3)^Mask_f)*Phase
     phase_H = process_3d.crop(phase_H,pad_size)
     return phase_L, Mask_f, phase_H
 
