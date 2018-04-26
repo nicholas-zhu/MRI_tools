3a33ad11b5aa455861e9d2562d00e0b3c55acdb8
xucheng.zhu@berkeley.edu <xucheng.zhu@berkeley.edu>
2018-04-25 20:10:58 -0700

    UPDATE IO function

--- IO/read_dcm.py
+++ IO/read_dcm.py
@@ -3,10 +3,17 @@ from __future__ import print_function
 import SimpleITK as sitk
 import sys, os
 
-def read_dcm(dicom_folder):
+def read_dcms(dicom_folder):
     print( "Reading Dicom directory:", dicom_folder)
     reader = sitk.ImageSeriesReader()
     dicom_names = reader.GetGDCMSeriesFileNames(dicom_folder)
     reader.SetFileNames(dicom_names)
-    image = reader.Execute()
+    image = sitk.GetArrayFromImage(reader.Execute())
+    return image
+
+def read_dcm(dicom_file):
+    reader = sitk.ImageFileReader()
+    reader.SetFileName(dicom_file)
+    image = sitk.GetArrayFromImage(reader.Execute())
+    
     return image
