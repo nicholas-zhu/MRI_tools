0fb273783bf978b799f9bfc337d26fa14069fdc2
nicholas-zhu <xucheng.zhu@berkeley.edu>
2018-04-26 20:51:54 -0700

    UPDATE fft with multi-threads

--- FFT/fft.py
+++ FFT/fft.py
@@ -1,5 +1,11 @@
 import numpy as np
 import pyfftw as pfft
+import multiprocessing
+
+N_max = multiprocessing.cpu_count()
+N_min = 1
+N = N_max//2
+N_threads = np.maximum(N,N_min)
 
 class fft:
     def __init__(self, shape, axes = (0,1,2)):
@@ -8,12 +14,12 @@ class fft:
     
     def FT(self,a):
         b = pfft.interfaces.numpy_fft.ifftshift(a,axes=self.axes)
-        b = pfft.interfaces.numpy_fft.fftn(b,axes=self.axes)
+        b = pfft.interfaces.numpy_fft.fftn(b,axes=self.axes,threads = N_threads)
         b = pfft.interfaces.numpy_fft.fftshift(b,axes=self.axes)
         return b
     
     def IFT(self,a):
         b = pfft.interfaces.numpy_fft.ifftshift(a,axes=self.axes)
-        b = pfft.interfaces.numpy_fft.ifftn(b, axes=self.axes)
+        b = pfft.interfaces.numpy_fft.ifftn(b, axes=self.axes,threads = N_threads)
         b = pfft.interfaces.numpy_fft.fftshift(b, axes=self.axes)
         return b
