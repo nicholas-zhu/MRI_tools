89efe924d2623ef339a71675103aace8097ef0b1
xucheng.zhu@berkeley.edu <xucheng.zhu@berkeley.edu>
2018-05-29 14:18:08 -0700

    UPDATE NUFFT folder

--- NUFFT/__init__.py
+++ NUFFT/__init__.py
@@ -4,4 +4,8 @@ from . import nufft_support
 from . import nufft_3d
 from . import nufft_gpu
 from . import nufft_util1
-from . import nufft_util2
+try:
+    import cupy as cp
+    from . import nufft_util2
+except ImportError:
+    print('No cuda module')
